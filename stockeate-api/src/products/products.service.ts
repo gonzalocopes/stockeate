8eef9e41cd4f44abdc48a3b4dfd194d2569a125e
lisandrovaldezz <lisandroval09@gmail.com>
2025-10-24 17:17:17 -0300

    endpoints activar/desactivar productos y editar productos

--- stockeate-api/src/products/products.service.ts
+++ stockeate-api/src/products/products.service.ts
@@ -1,6 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { ProductDto } from './products.controller';
+import { UpdateProductDto } from './products.controller';
 import { Decimal } from '@prisma/client/runtime/library';
 
 @Injectable()
@@ -68,21 +73,65 @@ export class ProductsService {
     });
   }
 
-  async updateProduct(code: string, branchId: string, product: ProductDto) {
-    return await this.prisma.product.update({
+  async updateProduct(
+    code: string,
+    branchId: string,
+    updateData: UpdateProductDto,
+  ) {
+    const product = await this.prisma.product.findFirst({
       where: { code, branchId },
-      data: {
-        name: product.name,
-        price: new Decimal(product.price),
-        stock: product.stock,
-      },
     });
+    if (!product) {
+      throw new NotFoundException(
+        `No se encontró producto con código ${code} en la sucursal indicada`,
+      );
+    }
+    const data: any = {};
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (updateData.code !== undefined) data.code = updateData.code;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (updateData.name !== undefined) data.name = updateData.name;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (updateData.stock !== undefined) data.stock = updateData.stock;
+    if (updateData.price !== undefined)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      data.price = new Decimal(updateData.price);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (updateData.isActive !== undefined) data.isActive = updateData.isActive;
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    if (Object.keys(data).length === 0) {
+      throw new BadRequestException('No hay campos válidos para actualizar');
+    }
+
+    const updated = await this.prisma.product.update({
+      where: { id: product.id },
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      data,
+    });
+
+    return updated;
   }
 
-  async markProductAsInactive(code: string, branchId: string) {
-    return await this.prisma.product.update({
-      where: { code, branchId },
-      data: { isActive: false },
+  async toggleActive(code: string, branchId: string) {
+    const product = await this.prisma.product.findFirst({
+      where: {
+        code,
+        branchId,
+      },
     });
+    if (!product) {
+      throw new NotFoundException('Producto no encontrado en esta sucursal');
+    }
+    const updatedProduct = await this.prisma.product.update({
+      where: {
+        id: product.id,
+      },
+      data: {
+        isActive: !product.isActive,
+      },
+    });
+
+    return updatedProduct;
   }
 }
